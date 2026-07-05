8d960592a1b87e0ecd5b753d46f4761f6c2583c8
Harsh Vardhan Singh <168753804+Harshvardhan312003@users.noreply.github.com>
2026-07-05 19:28:28 +0530

    Toaster component

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { Toaster } from 'react-hot-toast' // <-- IMPORT
+import { Toaster } from 'react-hot-toast' // <-- IMPORT 
 
 // --- IMPORT YOUR PROVIDERS ---
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
